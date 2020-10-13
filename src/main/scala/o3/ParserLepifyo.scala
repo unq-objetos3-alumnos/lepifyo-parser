fcbbfe34ccdec8c9a8187acd3c1735626599141b
AugustoConti <augustoconti10@gmail.com>
2020-10-12 22:18:41 -0300

    Parseo de booleanos, resta, division y comparaciones

--- src/main/scala/o3/ParserLepifyo.scala
+++ src/main/scala/o3/ParserLepifyo.scala
@@ -1,24 +1,38 @@
 package o3
+
 import scala.util.parsing.combinator._
 
 case class ParserLepifyo[TExpresion](
                                       numero: Int => TExpresion,
+                                      booleano: Boolean => TExpresion,
                                       suma: (TExpresion, TExpresion) => TExpresion,
-                                      multiplicacion: (TExpresion, TExpresion) => TExpresion
+                                      resta: (TExpresion, TExpresion) => TExpresion,
+                                      multiplicacion: (TExpresion, TExpresion) => TExpresion,
+                                      division: (TExpresion, TExpresion) => TExpresion,
+                                      igual: (TExpresion, TExpresion) => TExpresion,
+                                      distinto: (TExpresion, TExpresion) => TExpresion,
+                                      mayor: (TExpresion, TExpresion) => TExpresion,
+                                      mayorIgual: (TExpresion, TExpresion) => TExpresion,
+                                      menor: (TExpresion, TExpresion) => TExpresion,
+                                      menorIgual: (TExpresion, TExpresion) => TExpresion
                                     ) extends RegexParsers {
   def parsear(textoPrograma: String): TExpresion = {
     def parserNumero: Parser[TExpresion] = """[0-9]+""".r ^^ { n => numero(n.toInt) }
+    def parserBooleano: Parser[TExpresion] = "true" ^^^ booleano(true) | "false" ^^^ booleano(false)
 
-    def parserFactor = parserNumero
-    def parserMultiplicacion: Parser[TExpresion] = parserFactor ~ ("*" ~> parserFactor).+ ^^ {
-      case primerTermino ~ terminos => (terminos).foldLeft(primerTermino)(multiplicacion)
-    }
+    def parserFactor: Parser[TExpresion] = parserNumero | parserBooleano | "(" ~> parserExpresion <~ ")"
 
-    def parserTermino = parserMultiplicacion | parserNumero
-    def parserSuma: Parser[TExpresion] = parserTermino ~ ("+" ~> parserTermino).+ ^^ {
-      case primerTermino ~ terminos => (terminos).foldLeft(primerTermino)(suma)
-    }
-    def parserExpresion = parserSuma | parserMultiplicacion | parserNumero
+    def parserTermino = chainl1(parserFactor, "*" ^^^ multiplicacion | "/" ^^^ division)
+
+    def parserMiembros = chainl1(parserTermino, "+" ^^^ suma | "-" ^^^ resta)
+
+    def parserMiembroDesigualdad = chainl1(parserMiembros,
+      ">=" ^^^ mayorIgual |
+      "<=" ^^^ menorIgual |
+      ">" ^^^ mayor |
+      "<" ^^^ menor
+    )
+    def parserExpresion = chainl1(parserMiembroDesigualdad, "==" ^^^ igual | "!=" ^^^ distinto)
 
     parseAll(parserExpresion, textoPrograma) match {
       case Success(matched, _) => matched
--- src/test/scala/o3/ParserSpec.scala
+++ src/test/scala/o3/ParserSpec.scala
@@ -10,13 +10,35 @@ class ParserSpec extends AnyFunSpec with Matchers {
   class Expresion
 
   case class Numero(valor: Int) extends Expresion
+  case class Booleano(valor: Boolean) extends Expresion
 
   case class Suma(sumando1: Expresion, sumando2: Expresion) extends Expresion
-
+  case class Resta(minuendo: Expresion, sustraendo: Expresion) extends Expresion
   case class Multiplicacion(factor1: Expresion, factor2: Expresion) extends Expresion
+  case class Division(dividendo: Expresion, divisor: Expresion) extends Expresion
+
+  case class Igual(expresion1: Expresion, expresion2: Expresion) extends Expresion
+  case class Distinto(expresion1: Expresion, expresion2: Expresion) extends Expresion
+  case class Mayor(expresion1: Expresion, expresion2: Expresion) extends Expresion
+  case class MayorIgual(expresion1: Expresion, expresion2: Expresion) extends Expresion
+  case class Menor(expresion1: Expresion, expresion2: Expresion) extends Expresion
+  case class MenorIgual(expresion1: Expresion, expresion2: Expresion) extends Expresion
 
   describe("parsea") {
-    val parser = new ParserLepifyo[Expresion](numero = Numero, suma = Suma, multiplicacion = Multiplicacion)
+    val parser = new ParserLepifyo[Expresion](
+      numero = Numero,
+      booleano = Booleano,
+      suma = Suma,
+      resta = Resta,
+      multiplicacion = Multiplicacion,
+      division = Division,
+      igual = Igual,
+      distinto = Distinto,
+      mayor = Mayor,
+      mayorIgual = MayorIgual,
+      menor = Menor,
+      menorIgual = MenorIgual
+    )
 
     it("números literales") {
       val ast = parser.parsear("12")
@@ -47,5 +69,90 @@ class ParserSpec extends AnyFunSpec with Matchers {
 
       ast should equal(Multiplicacion(Multiplicacion(12, 34), 56))
     }
+
+    it("multiplicaciones de sumas") {
+      val ast = parser.parsear("12 * (34 + 56)")
+
+      ast should equal(Multiplicacion(12, Suma(34, 56)))
+    }
+
+    it("resta de numeros") {
+      val ast = parser.parsear("12 - 34 - 56")
+
+      ast should equal(Resta(Resta(12, 34), 56))
+    }
+
+    it("resta de suma a la derecha") {
+      val ast = parser.parsear("12 - 34 + 56")
+
+      ast should equal(Suma(Resta(12, 34), 56))
+    }
+
+    it("resta de suma a la izquierda") {
+      val ast = parser.parsear("12 + 34 - 56")
+
+      ast should equal(Resta(Suma(12, 34), 56))
+    }
+
+    it("division de numeros") {
+      val ast = parser.parsear("12 / 34 / 56")
+
+      ast should equal(Division(Division(12, 34), 56))
+    }
+
+    it("igualdad de numeros") {
+      val ast = parser.parsear("12 + 15 == 56 - 21")
+
+      ast should equal(Igual(Suma(12, 15), Resta(56, 21)))
+    }
+
+    it("desigualdad de numeros") {
+      val ast = parser.parsear("12 + 15 != 56 - 21")
+
+      ast should equal(Distinto(Suma(12, 15), Resta(56, 21)))
+    }
+
+    it("comparacion por mayor entre numeros") {
+      val ast = parser.parsear("12 + 15 > 56 - 21")
+
+      ast should equal(Mayor(Suma(12, 15), Resta(56, 21)))
+    }
+
+    it("comparacion por mayor igual entre numeros") {
+      val ast = parser.parsear("12 + 15 >= 56 - 21")
+
+      ast should equal(MayorIgual(Suma(12, 15), Resta(56, 21)))
+    }
+
+    it("comparacion por menor entre numeros") {
+      val ast = parser.parsear("12 + 15 < 56 - 21")
+
+      ast should equal(Menor(Suma(12, 15), Resta(56, 21)))
+    }
+
+    it("comparacion por menor igual entre numeros") {
+      val ast = parser.parsear("12 + 15 <= 56 - 21")
+
+      ast should equal(MenorIgual(Suma(12, 15), Resta(56, 21)))
+    }
+
+    it("igualdad entre comparaciones") {
+      val ast = parser.parsear("12 < 56 == 14 > 13")
+
+      ast should equal(Igual(Menor(12, 56), Mayor(14, 13)))
+    }
+
+    it("igualdad entre igualdades") {
+      val ast = parser.parsear("12 == 56 == 14")
+
+      ast should equal(Igual(Igual(12, 56), 14))
+    }
+
+    it("booleanos literales") {
+      val ast = parser.parsear("true == false")
+
+      ast should equal(Igual(Booleano(true), Booleano(false)))
+    }
+
   }
 }
